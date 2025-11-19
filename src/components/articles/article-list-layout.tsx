b4438984668f64df6abf27019cee0ea630863591
ManhVu-07 <manhvd1507@gmail.com>
2025-11-19 14:31:41 +0700

    fix: grid layout

--- src/components/articles/article-list-layout.tsx
+++ src/components/articles/article-list-layout.tsx
@@ -12,14 +12,12 @@ export function ArticleListLayout({
   sidePosts,
 }: ArticleListLayoutProps) {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-      {/* Main Article - Left Column */}
-      <div className="lg:col-span-2">
+    <div className="grid grid-cols-1 lg:grid-cols-5 gap-8">
+      <div className="lg:col-span-3">
         <MainArticleCard post={mainPost} />
       </div>
 
-      {/* Side Articles - Right Column */}
-      <div className="lg:col-span-1">
+      <div className="lg:col-span-2">
         <div className="space-y-6">
           {sidePosts.map((post) => (
             <ArticleListItem key={post._id || post.id} post={post} />
