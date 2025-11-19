3f79c5a0f45d1aaf25354a866089020d3f09b9c8
ManhVu-07 <manhvd1507@gmail.com>
2025-11-19 14:02:18 +0700

    add coin list section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import CoinSlider from "@/components/coin-slider";
+import CoinSlider from "@/components/coins/coin-slider";
 import { Footer } from "@/components/footer";
 import Header from "@/components/header";
 import { ArticleListSection } from "@/components/articles";
+import { CoinListsSection } from "@/components/coins";
 
 export default async function LandingPage() {
   return (
@@ -10,6 +11,7 @@ export default async function LandingPage() {
       <main className="space-y-12">
         <CoinSlider />
         <ArticleListSection mainPostLimit={1} sidePostsLimit={4} />
+        <CoinListsSection />
       </main>
       <Footer />
     </div>
