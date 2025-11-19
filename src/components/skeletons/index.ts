0dd0c86132789544a29f2e5e2fe4a522d5b971c9
ManhVu-07 <manhvd1507@gmail.com>
2025-11-19 11:50:46 +0700

    feat: add skeleton index

---
+++ src/components/skeletons/index.ts
@@ -0,0 +1,5 @@
+export { CoinSliderSkeleton } from "./coin-slider-skeleton";
+export { MainArticleCardSkeleton } from "./main-article-card-skeleton";
+export { ArticleListItemSkeleton } from "./article-list-item-skeleton";
+export { ArticleListLayoutSkeleton } from "./article-list-layout-skeleton";
+
